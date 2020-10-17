facb9b6aa4a8127473e2c886ff50e67fe31f3d98
Laura <laurabeatriserafim@gmail.com>
2020-10-17 19:28:49 -0300

    Add seo config with next-seo package

---
+++ config/seo.ts
@@ -0,0 +1,20 @@
+const seoConfig = {
+  title: "Floripa+ | Torne a ilha em um lugar melhor",
+  description: "Torne a ilha em um lugar melhor",
+  openGraph: {
+    url: "https://floripamais.com",
+    title: "Floripa+",
+    description: "Torne a ilha em um lugar melhor",
+    images: [
+      {
+        url: "https://floripamais.com/images/banner.png",
+        width: 1200,
+        height: 630,
+        alt: "Floripa+ | Torne a ilha em um lugar melhor",
+      },
+    ],
+    site_name: "Floripa+",
+  },
+};
+
+export default seoConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
+import { DefaultSeo } from "next-seo";
+
+import seoConfig from "config/seo";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <>
+    <DefaultSeo {...seoConfig} />
+    <Component {...pageProps} />
+  </>
 );
 
 export default App;
