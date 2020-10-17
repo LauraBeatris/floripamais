60a889f02bed48782186ec4257e23ef3962f2bd0
Laura <laurabeatriserafim@gmail.com>
2020-10-17 16:03:35 -0300

    Extend colors from default tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./components/*.{ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "orange-100": "#FB8F0A",
+        "orange-200": "#FC9414",
+        "background-light": "#F2F2F2",
+      },
+    },
+    fontFamily: {
+      body: ["Montserrat", "Arial", "sans-serif"],
+    },
   },
   variants: {},
   plugins: [],
