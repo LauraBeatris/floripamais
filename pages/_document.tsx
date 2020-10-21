81afffeeb6ab6e6dee90f63acb39d84cf5c6cfab
Laura <laurabeatriserafim@gmail.com>
2020-10-20 21:28:31 -0300

    Improve favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,22 +12,29 @@ class MyDocument extends Document {
         <Head>
           <meta title="Floripa+ | Torne a ilha em um lugar melhor" />
           <meta name="theme-color" content="#FFFFFF" />
-          <meta name="description" content="Torne a ilha em um lugar melhor" />
-          <link rel="icon" href="/favicons/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-icon-57x57.png" />
-          <link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-icon-60x60.png" />
-          <link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-icon-72x72.png" />
-          <link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-icon-76x76.png" />
-          <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-          <link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-icon-114x114.png" />
-          <link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-icon-120x120.png" />
-          <link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-icon-144x144.png" />
-          <link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-icon-152x152.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-icon-180x180.png" />
-          <link rel="icon" type="image/png" sizes="192x192" href="/favicons/android-icon-192x192.png" />
+          <link rel="icon" href="/favicons/favicon.ico" />
+          <meta name="application-name" content="&nbsp;" />
+          <meta name="msapplication-TileColor" content="#FFFFFF" />
+          <meta name="msapplication-TileImage" content="mstile-144x144.png" />
+          <meta name="description" content="Torne a ilha em um lugar melhor" />
+          <meta name="msapplication-square70x70logo" content="mstile-70x70.png" />
+          <meta name="msapplication-wide310x150logo" content="mstile-310x150.png" />
+          <meta name="msapplication-square310x310logo" content="mstile-310x310.png" />
+          <meta name="msapplication-square150x150logo" content="mstile-150x150.png" />
+          <link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96" />
+          <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+          <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+          <link rel="icon" type="image/png" href="favicon-128.png" sizes="128x128" />
+          <link rel="icon" type="image/png" href="favicon-196x196.png" sizes="196x196" />
+          <link rel="apple-touch-icon-precomposed" sizes="57x57" href="apple-touch-icon-57x57.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="72x72" href="apple-touch-icon-72x72.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="60x60" href="apple-touch-icon-60x60.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="76x76" href="apple-touch-icon-76x76.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="144x144" href="apple-touch-icon-144x144.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="120x120" href="apple-touch-icon-120x120.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="152x152" href="apple-touch-icon-152x152.png" />
+          <link rel="apple-touch-icon-precomposed" sizes="114x114" href="apple-touch-icon-114x114.png" />
         </Head>
         <body>
           <Main />
