12dfd919dceccf083e1b2d85818d4c7df7265887
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-03 19:35:23 -0300

    Remove self-hosted fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const fallbackFonts = ["Arial", "sans-serif"];
-
 module.exports = {
   purge: [
     "./pages/*.{ts,tsx}",
@@ -8,15 +6,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        black: "#0d0c0f",
         "orange-100": "#FB8F0A",
         "orange-200": "#FC9414",
         "background-light": "#F2F2F2",
       },
     },
     fontFamily: {
-      semibold: ["Montserrat-SemiBold", ...fallbackFonts],
-      medium: ["Montserrat-Medium", ...fallbackFonts],
-      light: ["Montserrat-Light", ...fallbackFonts],
+      body: ["Montserrat", "Arial", "sans-serif"],
     },
   },
   variants: {},
