4ae255f91eb48e3df8f830e325300351076acfde
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-03 19:37:51 -0300

    Add base header navbar links

---
+++ components/Header/constants.ts
@@ -0,0 +1,20 @@
+/**
+ * Defines the links to be show on the header navbar
+ */
+export const headerNavbarLinks = [
+  {
+    label: "Eventos",
+  },
+  {
+    label: "Doações",
+  },
+  {
+    label: "Missão",
+  },
+  {
+    label: "Equipe",
+  },
+  {
+    label: "Contato",
+  },
+];
