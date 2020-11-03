18d48687d99a43644928aef3436b808df5b5370a
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-03 19:39:23 -0300

    Add font-family link and context provider to root app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { AppProps } from "next/app";
 
 import "styles/main.css";
 
+import ContextProviders from "contexts";
 import seoConfig from "config/seo";
 import Header from "components/Header";
 
@@ -11,10 +12,13 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;600&display=swap" rel="stylesheet" />
     </Head>
     <DefaultSeo {...seoConfig} />
-    <Header />
-    <Component {...pageProps} />
+    <ContextProviders>
+      <Header />
+      <Component {...pageProps} />
+    </ContextProviders>
   </>
 );
 
