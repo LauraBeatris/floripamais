538c9a67c19eff91a51dd8e7b40b440c9de351f3
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-04 07:44:13 -0300

    Add default texts for footer

---
+++ components/Footer/constants.ts
@@ -0,0 +1,14 @@
+import { SocialMedia } from "./types";
+
+export const footerSocialMedias: SocialMedia[] = [
+  {
+    href: "https://www.instagram.com/floripa.mais/",
+    label: "Instagram",
+  },
+  {
+    href: "https://www.linkedin.com/company/floripa-mais/",
+    label: "Linkedin",
+  },
+];
+
+export const orgEmail = "organizacaofloripamais@gmail.com";
