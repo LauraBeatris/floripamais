bd061cb577d0076cc494b1ffedfcfa8b109d6e45
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-04 14:26:49 -0300

    Add animation to button element

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -8,6 +8,8 @@ const iconClassNames = "text-2xl ml-8";
 const buttonElementClassNames = "flex border-none outline-none focus:outline-none text-lg items-center bg-orange-100 hover:bg-orange-shade transition-colors duration-200 py-2 px-5 rounded-full";
 const buttonContainerClassNames = "flex pr-5 items-start justify-center py-0 border border-orange-100 hover:border-orange-shade rounded-full";
 
+const buttonElementHoverAnimation = "transform transition-transform duration-200 hover:translate-x-6";
+
 const buttonElementVariantClassNames = {
   [ButtonVariant.Primary]: "text-black",
   [ButtonVariant.Secondary]: "text-white",
@@ -21,8 +23,9 @@ const Button: React.FC<ButtonProps> = ({
   ...rest
 }) => {
   const concatenateButtonElementClassNames = clsx([
-    buttonElementClassNames,
     buttonElementClassName,
+    buttonElementClassNames,
+    buttonElementHoverAnimation,
     buttonElementVariantClassNames[variant],
   ]);
 
