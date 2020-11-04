32fee52b5a5b8b8439422fec1853501597fabcc4
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-04 17:26:20 -0300

    Remove unused classes from main page container

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => (
     <Head>
       <title>Floripa+ | Torne a ilha em um lugar melhor</title>
     </Head>
-    <main id="page-wrapper" className="h-screen w-screen pt-header-mobile md:pt-header-desktop flex flex-col justify-start bg-background-light">
+    <main id="page-wrapper" className="pt-header-mobile md:pt-header-desktop flex flex-col justify-start bg-background-light">
       <IntroductionSection />
     </main>
   </>
