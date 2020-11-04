2e9bfea3f32b1a3aa97f66e1be04175ac2ed5533
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-04 17:23:49 -0300

    Add default structure for the introduction section

---
+++ components/Sections/Introduction/Content.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Image from "next/image";
+import { Link as ScrollLink } from "react-scroll";
+
+import Button from "components/Button";
+import { ButtonVariant } from "components/Button/types";
+import { scrollDuration } from "constants/scroll";
+
+import { IntroductionSectionContentProps } from "./types";
+import { introductionSectionId, introductionSectionContent } from "./constants";
+
+const Content: React.FC<IntroductionSectionContentProps> = ({
+  backgroundSrc = introductionSectionContent.backgroundSrc,
+  buttonText = introductionSectionContent.buttonText,
+  title = introductionSectionContent.title,
+}) => (
+  <section id={introductionSectionId} className="flex items-center py-10 px-8 md:p-20 relative h-section-introduction bg-white">
+    <Image
+      src={backgroundSrc}
+      className="absolute object-cover h-full w-full"
+      layout="fill"
+      quality={100}
+      loading="eager"
+    />
+
+    <div className="absolute right-0 left-0 px-5">
+      <h1 className="text-white w-full md:w-30 font-bold text-4xl">
+        {title}
+      </h1>
+
+      {/* TODO -> Refer id of the mission section */}
+      <ScrollLink duration={scrollDuration} smooth to="mission-section">
+        <Button
+          variant={ButtonVariant.Secondary}
+          buttonContainerClassName="mt-10 w-full md:w-22"
+        >
+          {buttonText}
+        </Button>
+      </ScrollLink>
+    </div>
+  </section>
+);
+
+export default Content;
---
+++ components/Sections/Introduction/constants.ts
@@ -0,0 +1,8 @@
+export const introductionSectionContent = {
+  title: "Torne a ilha em um lugar melhor",
+  buttonText: "Conheça a nossa missão",
+  backgroundSrc: "https://images.unsplash.com/photo-1579920539896-b8b31ffb604a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+};
+
+export const introductionSectionId = "introduction-section";
+
