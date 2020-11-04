f5ad7ac1c34535862145c1dbed9aeadce524944a
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-04 14:04:46 -0300

    Add new color to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { shade } = require("polished");
+
 const purgeExtensions = "*.{js,ts,jsx,tsx}";
+
+const reuseColors = {
+  "orange-100": "#FB8F0A",
+};
 
 module.exports = {
   purge: {
@@ -17,8 +24,9 @@ module.exports = {
       colors: {
         black: "#0d0c0f",
         "yellow-100": "#DDA158",
-        "orange-100": "#FB8F0A",
+        "orange-100": reuseColors["orange-100"],
         "orange-200": "#FC9414",
+        "orange-shade": shade(0.2, reuseColors["orange-100"]),
         "background-light": "#F2F2F2",
       },
     },
