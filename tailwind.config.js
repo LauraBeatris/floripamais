12fb403a2ded87629c4404dc96baa344b56ba147
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-04 06:23:06 -0300

    Add new color to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     extend: {
       colors: {
         black: "#0d0c0f",
+        "yellow-100": "#DDA158",
         "orange-100": "#FB8F0A",
         "orange-200": "#FC9414",
         "background-light": "#F2F2F2",
