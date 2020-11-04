b11919c9ba4a2679aec1588d35a79e6eab6e6dc6
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-04 14:47:39 -0300

    Rollback app changes due to examples

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,11 @@
 import Head from "next/head";
 
-import Button from "components/Button";
-import { ButtonVariant } from "components/Button/types";
-
 const App: React.FC = () => (
   <>
     <Head>
       <title>Floripa+ | Torne a ilha em um lugar melhor</title>
     </Head>
-    <main id="page-wrapper" className="flex flex-col h-screen w-screen justify-center items-center bg-background-light">
-      <Button variant={ButtonVariant.Primary} buttonContainerClassName="mb-10">Primary</Button>
-
-      <Button variant={ButtonVariant.Secondary}>Secondary</Button>
-    </main>
+    <main id="page-wrapper" className="h-screen w-screen flex justify-center items-center bg-background-light" />
   </>
 );
 
