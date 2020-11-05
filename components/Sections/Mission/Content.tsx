e699193e0574460b029fca733dcd86045c91ea5a
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-05 19:56:49 -0300

    Refactor mission section with generic section component

--- components/Sections/Mission/Content.tsx
+++ components/Sections/Mission/Content.tsx
@@ -1,34 +1,30 @@
 import React from "react";
-import Emoji from "a11y-react-emoji";
-
-import withMaxWidthContainer from "hocs/withMaxWidthContainer";
 
 import { missionSectionContent, missionSectionId } from "./constants";
 import { MissionSectionContentProps } from "./types";
+import Section from "../Section";
+import { SectionVariant } from "../Section/types";
 
 const MissionSectionContent: React.FC<MissionSectionContentProps> = ({
   content = missionSectionContent,
 }) => (
   <section
     id={missionSectionId}
-    className="flex flex-col bg-white-shade p-10 md:p-20"
+    className="flex flex-col bg-white-shade"
   >
     {
       content.map(({ title, emoji, text }) => (
-        <section key={title} className="mb-16 last:mb-0 flex flex-col items-center">
-          <h1 className="text-center text-black font-bold text-3xl">
-            {title}
-            {" "}
-            <Emoji symbol={emoji} />
-          </h1>
-
-          <h2 className="text-center text-black font-medium text-2xl mt-8">
-            {text}
-          </h2>
-        </section>
+        <Section
+          key={title}
+          text={text}
+          title={title}
+          variant={SectionVariant.Primary}
+          emojiSymbol={emoji}
+          className="pt-0 first:pt-16"
+        />
       ))
     }
   </section>
 );
 
-export default withMaxWidthContainer(MissionSectionContent);
+export default MissionSectionContent;
