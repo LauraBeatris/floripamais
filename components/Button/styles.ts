8b5e4b259b3ed52001d4d31c5fcf22caf4a99735
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-06 09:58:30 -0300

    Add button wrapper

--- components/Button/styles.ts
+++ components/Button/styles.ts
@@ -3,6 +3,8 @@ import { ButtonVariant } from "./types";
 const buttonStyles = {
   icon: "text-2xl ml-8",
 
+  wrapper: "inline-block",
+
   element: "flex flex-1 justify-between flex-wrap border-none outline-none focus:outline-none text-xs md:text-lg items-center bg-orange-100 hover:bg-orange-shade transition-colors duration-200 py-2 px-5 rounded-full",
 
   container: "flex pr-5 items-start justify-center py-0 border border-orange-100 hover:border-orange-shade rounded-full",
