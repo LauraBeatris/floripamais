193e766d1a94ff04ca56ccae5cb027e2c50eae0c
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-06 10:02:22 -0300

    Add donations section

---
+++ components/Sections/Donations/constants.ts
@@ -0,0 +1,7 @@
+export const donationsSectionContent = {
+  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis sit massa id fermentum pellentesque rhoncus. Vestibulum cursus eu sed morbi volutpat. Adipiscing commodo, ac fusce tellus nisi, sapien eget. Interdum et sed ut pellentesque in aliquam gravida leo.",
+  title: "Transparência de Doações",
+  emojiSymbol: "🔈",
+  buttonText: "Visualizar doações",
+  buttonLink: "https://docs.google.com/spreadsheets/d/1atVMG0EHRiqqIkoIDa1-cE1GIJ1Hg71otC2q7o6XjTg/edit?usp=sharing",
+};
