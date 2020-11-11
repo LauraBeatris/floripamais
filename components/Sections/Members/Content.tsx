7c574bf4764bb43f5ca71787f157f2d11ef5dd3b
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-11 10:31:38 -0300

    Add id to actions section and members section

--- components/Sections/Members/Content.tsx
+++ components/Sections/Members/Content.tsx
@@ -4,14 +4,18 @@ import MembersSlider from "components/Slider/MembersSlider";
 import Section from "components/Sections/Section";
 
 import { MembersSectionContentProps } from "./types";
-import { membersSectionContent } from "./constants";
+import { membersSectionContent, membersSectionId } from "./constants";
 
 const MembersSectionContent: React.FC<MembersSectionContentProps> = ({
   text = membersSectionContent.text,
   title = membersSectionContent.title,
   members = membersSectionContent.members,
 }) => (
-  <Section title={title} text={text}>
+  <Section
+    id={membersSectionId}
+    title={title}
+    text={text}
+  >
     <div className="w-full mt-12">
       <MembersSlider members={members} />
     </div>
