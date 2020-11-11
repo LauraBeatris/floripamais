8305b41f77777819d029c4dc19958ff78b6ae651
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-11 10:10:05 -0300

    Add members slider component

---
+++ components/Slider/MembersSlider/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Image from "next/image";
+
+import Slider from "components/Slider";
+
+import { MembersSliderProps } from "./types";
+
+const MembersSlider: React.FC<MembersSliderProps> = ({
+  members,
+}) => (
+  <Slider autoplay infinite speed={5000}>
+    {members.map(({
+      id,
+      name,
+      photoUrl,
+    }) => (
+      <div className="py-2" key={id}>
+        <div className="shadow-md min-h-slider-item text-left flex flex-col justify-between items-start text-black bg-white-light rounded-lg mx-2 p-5">
+          <header className="w-full">
+            <h5 className="text-2xl font-semibold">
+              {name}
+            </h5>
+          </header>
+
+          <div className="flex-grow relative w-full h-full mt-2">
+            <Image
+              src={photoUrl}
+              alt={name}
+              className="rounded-lg absolute top-0 bottom-0 right-0 left-0 w-full h-full object-cover"
+              layout="fill"
+            />
+          </div>
+        </div>
+      </div>
+    ))}
+  </Slider>
+);
+
+export default MembersSlider;
---
+++ components/Slider/MembersSlider/types.ts
@@ -0,0 +1,5 @@
+import { Member } from "components/Sections/Members/types";
+
+export interface MembersSliderProps {
+  members: Member[];
+}
