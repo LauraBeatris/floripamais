7d4f74c25dbc7c4e78f7ea07c97c8c7ff337b3e5
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-11 18:47:28 -0300

    Update button font-size on smaller devices

--- components/Button/styles.ts
+++ components/Button/styles.ts
@@ -3,7 +3,7 @@ const buttonStyles = {
 
   wrapper: "inline-block",
 
-  element: "flex text-black flex-1 justify-between flex-wrap border-none text-xs md:text-lg font-semibold items-center bg-orange-100 hover:bg-orange-shade transition-colors duration-200 py-2 px-5 rounded-full",
+  element: "flex text-black flex-1 justify-between flex-wrap border-none text-base md:text-lg font-semibold items-center bg-orange-100 hover:bg-orange-shade transition-colors duration-200 py-2 px-5 rounded-full",
 
   container: "flex pr-5 items-start justify-center py-0 border border-orange-100 hover:border-orange-shade rounded-full",
 
