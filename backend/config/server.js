a275beec56fb17e31229ed0dfbad2c41f0c73384
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-12 09:31:00 -0300

    Add backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7640120e846fed20d4c0dc24f49c8bb0'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '45fa7028-627e-44f5-92be-f72482e73f63'
+};
