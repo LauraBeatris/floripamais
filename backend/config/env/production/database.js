3df3e039cc474b8bfe52dc0ab44213c884403324
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-13 12:52:25 -0300

    Update database config

---
+++ backend/config/env/production/database.js
@@ -0,0 +1,22 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+      },
+      options: {
+        ssl: false,
+      },
+    },
+  },
+});
