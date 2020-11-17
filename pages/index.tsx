474a9fe5d4a3f950d2538a5b022f0df7da79438c
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-17 18:30:51 -0300

    Fetch main page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,45 +8,11 @@ import DonationsSection from "components/Sections/Donations";
 import ActionsSection from "components/Sections/Actions";
 import TeamMembersSection from "components/Sections/TeamMembers";
 import Footer from "components/Footer";
-import getIntroductionSection from "queries/getIntroductionSection";
-import getAchievementsSection from "queries/getAchievementsSection";
-import getDonationsSection from "queries/getDonationsSection";
-import getActionsSection from "queries/getActionsSection";
-import getTeamMembersSection from "queries/getTeamMembersSection";
-import getOrgInfo from "queries/getOrgInfo";
-import getFooter from "queries/getFooter";
+import getMainPageContent from "queries/getMainPageContentQuery";
 
 export const getStaticProps = async () => {
-  const introductionSection = (
-    await getIntroductionSection()
-  );
-
-  const achievementsSection = (
-    await getAchievementsSection()
-  );
-
-  const donationsSection = (
-    await getDonationsSection()
-  );
-
-  const actionsSection = (
-    await getActionsSection()
-  );
-
-  const teamMembersSection = (
-    await getTeamMembersSection()
-  );
-
-  const orgInfos = (
-    await getOrgInfo()
-  );
-
-  const footer = (
-    await getFooter()
-  );
-
-  return {
-    props: {
+  try {
+    const {
       footer,
       orgInfos,
       actionsSection,
@@ -54,8 +20,27 @@ export const getStaticProps = async () => {
       teamMembersSection,
       introductionSection,
       achievementsSection,
-    },
-  };
+    } = await getMainPageContent();
+
+    return {
+      props: {
+        footer,
+        orgInfos,
+        actionsSection,
+        donationsSection,
+        teamMembersSection,
+        introductionSection,
+        achievementsSection,
+      },
+    };
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+
+    return {
+      props: {},
+    };
+  }
 };
 
 const MainPage: React.FC< InferGetStaticPropsType<typeof getStaticProps>> = ({
