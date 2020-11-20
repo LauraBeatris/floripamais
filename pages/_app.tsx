2204d89add8607da2f49392defe86eaf844906f1
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-20 09:37:58 -0300

    Remove google font host

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,6 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;600&display=swap" rel="stylesheet" />
     </Head>
     <DefaultSeo {...seoConfig} />
     <ContextProviders>
