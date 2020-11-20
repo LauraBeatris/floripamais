359941e8073c3874d41c47b64912d44bb7243311
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-20 15:49:00 -0300

    Remove unnecessary empty string and fix typo

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,13 +4,11 @@ import Link from "next/link";
 const Page404: React.FC = () => (
   <div className="flex h-full items-center justify-center pt-20  h-full-screen-mobile md:h-full-screen-desktop">
     <h1 className="text-3xl w-1/2 text-center">
-      Ops... Essa pagina não existe. Clique
+      Ops... Essa página não existe. Clique
       {" "}
       <Link href="/"><span className="text-orange-200 cursor-pointer hover:underline">aqui</span></Link>
       {" "}
-      e
-      {" "}
-      vá para a pagina principal
+      e vá para a pagina principal
     </h1>
   </div>
 );
