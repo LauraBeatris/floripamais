b215d9caad3a721867b7fcd9e0b6c52a71bf40c1
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-21 19:07:07 -0300

    Merge branch 'main' of https://github.com/LauraBeatris/floripamais into main

--- components/Sections/Introduction/constants.ts
+++ components/Sections/Introduction/constants.ts
@@ -1,7 +1,6 @@
 export const introductionSectionFixtures = {
   title: "Torne a ilha em um lugar melhor",
   buttonText: "Conheça a nossa missão",
-  backgroundImageLink: "https://images.unsplash.com/photo-1597339626928-b9d33757c7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&q=80",
 };
 
 export const introductionSectionId = "introduction-section";
