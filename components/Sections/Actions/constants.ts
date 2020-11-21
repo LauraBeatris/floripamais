f5dbec36ad10289431ad609d96a9ef47c25fa891
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-21 16:12:59 -0300

    Add next and previous actions on actions section constants

--- components/Sections/Actions/constants.ts
+++ components/Sections/Actions/constants.ts
@@ -1,36 +1,47 @@
-const nextActions = [
+import { addDays, subDays } from "date-fns";
+
+const now = Date.now();
+
+const actions = [
   {
     id: "1",
-    name: "Ação 1",
-    date: new Date(2020, 5, 1),
+    name: "Ação 4",
+    date: addDays(now, 1),
     actionLink: "https://www.instagram.com/p/CFX0OqcFb0Z/",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis sit massa id.",
   },
   {
     id: "2",
-    name: "Ação 2",
-    date: new Date(2020, 5, 2),
+    name: "Ação 5",
+    date: addDays(now, 2),
     actionLink: "https://www.instagram.com/p/CFX0OqcFb0Z/",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis sit massa id.",
   },
   {
     id: "3",
-    name: "Ação 3",
-    date: new Date(2020, 5, 3),
+    name: "Ação 6",
+    date: addDays(now, 3),
     actionLink: "https://www.instagram.com/p/CFX0OqcFb0Z/",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis sit massa id.",
   },
   {
     id: "4",
-    name: "Ação 4",
-    date: new Date(2020, 5, 4),
+    name: "Ação 1",
+    date: subDays(now, 3),
     actionLink: "https://www.instagram.com/p/CFX0OqcFb0Z/",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis sit massa id.",
   },
   {
     id: "5",
-    name: "Ação 5",
-    date: new Date(2020, 5, 5),
+    name: "Ação 2",
+    date: subDays(now, 2),
+    actionLink: "https://www.instagram.com/p/CFX0OqcFb0Z/",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis sit massa id.",
+  },
+  {
+    id: "6",
+    name: "Ação 3",
+    date: subDays(now, 1),
     actionLink: "https://www.instagram.com/p/CFX0OqcFb0Z/",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis sit massa id.",
   },
@@ -39,10 +50,11 @@ const nextActions = [
 export const actionsSectionFixtures = {
   text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis sit massa id fermentum pellentesque rhoncus. Vestibulum cursus eu sed morbi volutpat. Adipiscing commodo, ac fusce tellus nisi, sapien eget. Interdum et sed ut pellentesque in aliquam gravida leo.",
   title: "Ações",
-  nextActions,
+  actions,
   actionButtonText: "Comparecer",
   noActionsFoundText: "Não há ações no momento, mas nos siga nas redes sociais para mais informaçöes",
   nextActionsSubtitle: "Próximas ações",
+  previousActionsSubtitle: "Ações anteriores",
 };
 
 export const actionsSectionId = "actions-section";
