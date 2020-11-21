00cf468b2778f8bdd4a9522b0cba4353f37702b9
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-11-21 16:13:42 -0300

    Show previous and next actions on actions section

--- components/Sections/Actions/types.ts
+++ components/Sections/Actions/types.ts
@@ -1,3 +1,10 @@
 import { GetMainPageSectionQuery } from "generated/graphql";
 
 export type ActionsSectionProps = GetMainPageSectionQuery["actionsSection"];
+
+export interface ActionsSliderContainerProps {
+  title: string;
+  actions: ActionsSectionProps["actions"];
+  actionButtonText: ActionsSectionProps["actionButtonText"];
+  shouldShowNoActionsMessage?: boolean;
+}
